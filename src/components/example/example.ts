12fd773363fb1b2287c0bf3489fc7648eb7f19a2
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-02-13 12:23:55 +0300

    COREUI-133 common: init

---
+++ src/components/example/example.ts
@@ -0,0 +1,7 @@
+// TODO: This is demo example. Please, remove it.
+export class Example {
+
+    static getTimestamp(date: string): number {
+        return new Date(date).getTime();
+    }
+}
---
+++ webpack.config.package.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const DtsBundlePlugin = require('dts-bundle/lib/dts-bundle-webpack');
+
+const packageJson = fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf8');
+const libraryName = JSON.parse(packageJson).name;
+
+module.exports = {
+    entry: './src/package-api.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        library: libraryName,
+        libraryTarget: 'commonjs2'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('./dist/*'),
+        new DtsBundlePlugin({
+            name: libraryName,
+            // Because a dts bundle module clear 2 last symbols before splitting
+            baseDir: './dist//',
+            main: './dist/types/**/*.d.ts',
+            out: './index.d.ts'
+        }),
+        new CopyWebpackPlugin([
+            { from: 'package.json', to: 'package.json' }
+        ])
+    ]
+};
